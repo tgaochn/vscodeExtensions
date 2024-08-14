5777bc6e68826b40f2a91f172fb4f0119d031486
5800X\gtfish <tgaochn@gmail.com>
2024-08-14 00:16:18 -0500

    gtfish24_jira_markdown: 改进功能

--- gtfish24_jira_markdown/mod/extension/README.md
+++ gtfish24_jira_markdown/mod/extension/README.md
@@ -2,6 +2,11 @@ forked version from chintans98.markdown-jira-1.1.0.
 
 # changelog
 
+## [0.0.4] - 2024-08-14
+
+1. 转化选中的md文本而不是整个文件, 避免md文件太长时速度太慢
+2. 转化后的内容写入一个侧边栏的临时tab而不是生成新的文件, 避免产生太多垃圾文件
+
 ## [0.0.3] - 2024-07-24
 
 1. `text` -> {color:#DE350B}text{color}
--- gtfish24_jira_markdown/mod/extension/extension.js
+++ gtfish24_jira_markdown/mod/extension/extension.js
@@ -1,10 +1,6 @@
 const vscode = require('vscode');
 const path = require('path');
 
-/**
- * Helper functions below
- */
-
 function getCurrentDirectory(document) {
     return path.dirname(document.uri.path);
 }
@@ -73,6 +69,22 @@ function openNewDocumentWithConvertedText(directory, newFileText, fileExtension)
         });
 }
 
+function openNewDocumentInSplitView(newFileText) {
+    // Create an untitled document
+    const document_new = vscode.workspace.openTextDocument({
+        content: newFileText,
+        language: 'plaintext'
+    });
+
+    // Show the document in a new editor tab
+    const editor_new = vscode.window.showTextDocument(document_new, {
+        preview: false,
+        viewColumn: vscode.ViewColumn.Beside
+    });
+
+    vscode.window.showTextDocument(editor_new.document, editor_new.viewColumn, false);
+}
+
 function toMarkdownString(text) {
     return text.replace(/^[ \t]*(\*+)\s+/gm, function (match, stars) {
         return Array(stars.length).join("  ") + '* ';
@@ -126,104 +138,6 @@ function toMarkdownString(text) {
         .replace(/^[ \t]*\|/gm, '|');
 
 }
-
-function toMarkdownString2(input) {
-    // function from https://github.com/FokkeZB/J2M/blob/master/src/J2M.js
-    // !! seems not working properly
-
-    input = input.replace(/^bq\.(.*)$/gm, function (match, content) {
-        return '> ' + content + "\n";
-    });
-
-    input = input.replace(/([*_])(.*)\1/g, function (match, wrapper, content) {
-        var to = (wrapper === '*') ? '**' : '*';
-        return to + content + to;
-    });
-
-    // multi-level numbered list
-    input = input.replace(/^((?:#|-|\+|\*)+) (.*)$/gm, function (match, level, content) {
-        var len = 2;
-        var prefix = '1.';
-        if (level.length > 1) {
-            len = parseInt((level.length - 1) * 4) + 2;
-        }
-
-        // take the last character of the level to determine the replacement
-        var prefix = level[level.length - 1];
-        if (prefix == '#') prefix = '1.';
-
-        return Array(len).join(" ") + prefix + ' ' + content;
-    });
-
-    // headers, must be after numbered lists
-    input = input.replace(/^h([0-6])\.(.*)$/gm, function (match, level, content) {
-        return Array(parseInt(level) + 1).join('#') + content;
-    });
-
-    input = input.replace(/\{\{([^}]+)\}\}/g, '`$1`');
-    input = input.replace(/\?\?((?:.[^?]|[^?].)+)\?\?/g, '<cite>$1</cite>');
-    input = input.replace(/\+([^+]*)\+/g, '<ins>$1</ins>');
-    input = input.replace(/\^([^^]*)\^/g, '<sup>$1</sup>');
-    input = input.replace(/~([^~]*)~/g, '<sub>$1</sub>');
-    input = input.replace(/-([^-]*)-/g, '-$1-');
-
-    input = input.replace(/\{code(:([a-z]+))?\}([^]*?)\{code\}/gm, '```$2$3```');
-    input = input.replace(/\{quote\}([^]*)\{quote\}/gm, function (match, content) {
-        lines = content.split(/\r?\n/gm);
-
-        for (var i = 0; i < lines.length; i++) {
-            lines[i] = '> ' + lines[i];
-        }
-
-        return lines.join("\n");
-    });
-
-    // Images with alt= among their parameters
-    input = input.replace(/!([^|\n\s]+)\|([^\n!]*)alt=([^\n!\,]+?)(,([^\n!]*))?!/g, '![$3]($1)');
-    // Images with just other parameters (ignore them)
-    input = input.replace(/!([^|\n\s]+)\|([^\n!]*)!/g, '![]($1)');
-    // Images without any parameters or alt
-    input = input.replace(/!([^\n\s!]+)!/g, '![]($1)');
-
-    input = input.replace(/\[([^|]+)\|(.+?)\]/g, '[$1]($2)');
-    input = input.replace(/\[(.+?)\]([^\(]+)/g, '<$1>$2');
-
-    input = input.replace(/{noformat}/g, '```');
-    input = input.replace(/{color:([^}]+)}([^]*?){color}/gm, '<span style="color:$1">$2</span>');
-
-    // Convert header rows of tables by splitting input on lines
-    lines = input.split(/\r?\n/gm);
-    lines_to_remove = []
-    for (var i = 0; i < lines.length; i++) {
-        line_content = lines[i];
-
-        seperators = line_content.match(/\|\|/g);
-        if (seperators != null) {
-            lines[i] = lines[i].replace(/\|\|/g, "|");
-            console.log(seperators)
-
-            // Add a new line to mark the header in Markdown,
-            // we require that at least 3 -'s are between each |
-            header_line = "";
-            for (var j = 0; j < seperators.length - 1; j++) {
-                header_line += "|---";
-            }
-
-            header_line += "|";
-
-            lines.splice(i + 1, 0, header_line);
-
-        }
-    }
-
-    // Join the split lines back
-    input = ""
-    for (var i = 0; i < lines.length; i++) {
-        input += lines[i] + "\n"
-    }
-
-    return input;
-};
 
 function toMarkdown() {
     let document = vscode.window.activeTextEditor.document;
@@ -292,7 +206,9 @@ function toJiraString(text) {
 
         // Inline-Preformatted Text
         // .replace(/`([^`]+)`/g, '{{$1}}') // `text` -> {{text}}
-        .replace(/`([^`]+)`/g, '{color:#DE350B}$1{color}') // `text` -> {color:#DE350B}text{color}
+        // .replace(/`([^`]+)`/g, '{color:#DE350B}$1{color}') // `text` -> {color:#DE350B}text{color}
+        // .replace(/`([^`]+)`/g, '{color:#FFE536}$1{color}') // `text` -> {color:#FFE536}text{color}
+        .replace(/`([^`]+)`/g, '{color:#77FF92}$1{color}') // `text` -> {color:#77FF92}text{color}
 
         // Named Link: [text](url) -> [text|url]
         .replace(/\[([^\]]+)\]\(([^)]+)\)/g, '[$1|$2]')
@@ -329,124 +245,17 @@ function toJiraString(text) {
             });
 }
 
-function toJiraString2(input) {
-    // function from https://github.com/FokkeZB/J2M/blob/master/src/J2M.js
-    // !! seems not working properly
-
-    // remove sections that shouldn't be recursively processed
-    var START = 'J2MBLOCKPLACEHOLDER';
-    var replacementsList = [];
-    var counter = 0;
-
-    input = input.replace(/`{3,}(\w+)?((?:\n|.)+?)`{3,}/g, function (match, synt, content) {
-        var code = '{code';
-
-        if (synt) {
-            code += ':' + synt;
-        }
-
-        code += '}' + content + '{code}';
-        var key = START + counter++ + '%%';
-        replacementsList.push({ key: key, value: code });
-        return key;
-    });
-
-    input = input.replace(/`([^`]+)`/g, function (match, content) {
-        var code = '{{' + content + '}}';
-        var key = START + counter++ + '%%';
-        replacementsList.push({ key: key, value: code });
-        return key;
-    });
-
-    input = input.replace(/`([^`]+)`/g, '{{$1}}');
-
-    input = input.replace(/^(.*?)\n([=-])+$/gm, function (match, content, level) {
-        return 'h' + (level[0] === '=' ? 1 : 2) + '. ' + content;
-    });
-
-    input = input.replace(/^([#]+)(.*?)$/gm, function (match, level, content) {
-        return 'h' + level.length + '.' + content;
-    });
-
-    input = input.replace(/([*_]+)(.*?)\1/g, function (match, wrapper, content) {
-        var to = (wrapper.length === 1) ? '_' : '*';
-        return to + content + to;
-    });
-
-    // multi-level bulleted list
-    input = input.replace(/^(\s*)- (.*)$/gm, function (match, level, content) {
-        var len = 2;
-        if (level.length > 0) {
-            len = parseInt(level.length / 4.0) + 2;
-        }
-        return Array(len).join("-") + ' ' + content;
-    });
-
-    // multi-level numbered list
-    input = input.replace(/^(\s+)1. (.*)$/gm, function (match, level, content) {
-        var len = 2;
-        if (level.length > 1) {
-            len = parseInt(level.length / 4) + 2;
-        }
-        return Array(len).join("#") + ' ' + content;
-    });
-
-    var map = {
-        cite: '??',
-        del: '-',
-        ins: '+',
-        sup: '^',
-        sub: '~'
-    };
-
-    input = input.replace(new RegExp('<(' + Object.keys(map).join('|') + ')>(.*?)<\/\\1>', 'g'), function (match, from, content) {
-        //console.log(from);
-        var to = map[from];
-        return to + content + to;
-    });
-
-    input = input.replace(/<span style="color:(#[^"]+)">([^]*?)<\/span>/gm, '{color:$1}$2{color}');
-
-    input = input.replace(/~~(.*?)~~/g, '-$1-');
-
-    // Images without alt
-    input = input.replace(/!\[\]\(([^)\n\s]+)\)/g, '!$1!');
-    // Images with alt
-    input = input.replace(/!\[([^\]\n]+)\]\(([^)\n\s]+)\)/g, '!$2|alt=$1!');
-
-    input = input.replace(/\[([^\]]+)\]\(([^)]+)\)/g, '[$1|$2]');
-    input = input.replace(/<([^>]+)>/g, '[$1]');
-
-    // restore extracted sections
-    for (var i = 0; i < replacementsList.length; i++) {
-        var sub = replacementsList[i];
-        input = input.replace(sub["key"], sub["value"]);
-    }
-
-    // Convert header rows of tables by splitting input on lines
-    lines = input.split(/\r?\n/gm);
-    lines_to_remove = []
-    for (var i = 0; i < lines.length; i++) {
-        line_content = lines[i];
-
-        if (line_content.match(/\|---/g) != null) {
-            lines[i - 1] = lines[i - 1].replace(/\|/g, "||")
-            lines.splice(i, 1)
-        }
-    }
-
-    // Join the split lines back
-    input = ""
-    for (var i = 0; i < lines.length; i++) {
-        input += lines[i] + "\n"
-    }
-    return input;
-};
-
 function toJira() {
-    let document = vscode.window.activeTextEditor.document;
-    let jiraFormatted = toJiraString(document.getText());
-    openNewDocumentWithConvertedText(getCurrentFilePath(), jiraFormatted, ".jira");
+    let editor = vscode.window.activeTextEditor;
+    if (!editor) {
+        return;
+    }
+    let document = editor.document;
+    let selectionTextString = document.getText(editor.selection);
+
+    let jiraFormatted = toJiraString(selectionTextString);
+    // openNewDocumentWithConvertedText(getCurrentFilePath(), jiraFormatted, ".jira"); // 把jira内容放在新的文件里
+    openNewDocumentInSplitView(jiraFormatted); // 把jira内容放在侧边栏的临时tab里
 }
 
 // Method is called when the extension is first activated
